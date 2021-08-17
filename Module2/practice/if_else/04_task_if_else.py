9214cf1e6ace77cb401bf5f56e913541b4275cfb
MurrainG <89096778+MurrainG@users.noreply.github.com>
2021-08-17 21:07:14 +0300

    Update 04_task_if_else.py

--- Module2/practice/if_else/04_task_if_else.py
+++ Module2/practice/if_else/04_task_if_else.py
@@ -1,7 +1,7 @@
-# Дано целое число.
-# Если оно делится на 3 без остатка - вывести ”Foo”,
-# если делится на 5 - вывести “Bar”,
-# а если делится на 3 и на 5 - вывести “Foobar”.
-# Для всех остальных случаев не выводить ничего.
-
-# TODO: your code here
+number = int(input())
+if number % 3 == 0 and number % 5 == 0:
+    print("FooBar")
+if number % 3 == 0 and number % 5 != 0:
+    print("Foo")
+if number % 3 != 0 and number % 5 == 0:
+    print("Bar")

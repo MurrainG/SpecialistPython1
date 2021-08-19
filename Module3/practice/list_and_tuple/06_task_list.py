6224269b2411294f83d0a4e00bba01d37932e1c7
MurrainG <89096778+MurrainG@users.noreply.github.com>
2021-08-19 19:30:18 +0300

    Update 06_task_list.py

--- Module3/practice/list_and_tuple/06_task_list.py
+++ Module3/practice/list_and_tuple/06_task_list.py
@@ -5,3 +5,11 @@ first_number = int(input())     # Первое число
 second_number = int(input())    # Второе число
 
 # TODO: your code here
+
+first_number = int(input())
+second_number = int(input())
+new_list = []
+for ran in range(first_number, second_number):
+    if ran % 3 == 0:
+        new_list.append(ran)
+print(new_list)

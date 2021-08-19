c37001242ecf123a83ac2781dc1fdf688a8ce7df
MurrainG <89096778+MurrainG@users.noreply.github.com>
2021-08-19 19:07:40 +0300

    Update 04_task_list.py

--- Module3/practice/list_and_tuple/04_task_list.py
+++ Module3/practice/list_and_tuple/04_task_list.py
@@ -2,3 +2,15 @@
 # Вывести на экран сумму всех положительных элементов.
 
 # TODO: your code here
+
+import random
+sum = 0
+random_el = []
+n = random.randint(10,1500)
+for i in range (1,n):
+    random_number = random.randint(-10,10)
+    random_el.append( random_number)
+for el in random_el:
+    if el > 0:
+        sum += el
+print(sum)

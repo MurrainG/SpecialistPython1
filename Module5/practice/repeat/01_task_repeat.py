234e8ec3310de3bfc7e1b6b10d109fdeb0da83aa
MurrainG <89096778+MurrainG@users.noreply.github.com>
2021-08-23 19:26:19 +0300

    Update 01_task_repeat.py

--- Module5/practice/repeat/01_task_repeat.py
+++ Module5/practice/repeat/01_task_repeat.py
@@ -1,7 +1,13 @@
-# Напишите функцию, создающую(возвращающую) список заданной длины заполненный
-# произвольными целыми числами в заданном диапазоне.
-# , где size - размер генерируемого списка c элементами в диапазоне от of до to.
+import random
+
 
 def gen_list(size, of, to):
-    pass
+    new_list = []
+    for _ in range(size):
+        number = random.randint(of, to)
+        new_list.append(number)
+    return new_list
+
+
+print(gen_list(123, -1293120, 1276351))
 
